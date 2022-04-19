680a63428754ee638686f46b4b1435fd3d4dba0d
Kale Leach <kleach1212@gmail.com>
2022-04-19 16:39:42 -0700

    commit tree 2bf80b75cd02a4ab8686830c08e22007159e7493

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,25 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+
+  const movieArr = movies.map((movie) => (
+    <div key = {movie.time}>
+      <h2>{movie.title}</h2>
+      <div>{movie.time}</div>
+      <ul>
+          {movie.genres.map((genre) => 
+            <li key = {genre}>{genre}</li>
+          )}
+      </ul>
+    </div>
+  ))
+  
+  return (
+  <div>
+    <h1>Movies Page</h1>
+    {movieArr}
+  </div>
+  )
 }
 
 export default Movies;
